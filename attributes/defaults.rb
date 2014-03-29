95b7460ac27a24d5a6e1cb1129de9cafd2357f61
James Pennell <pennell.james@gmail.com>
2014-03-28 23:54:39 -0400

    Move data directory

--- attributes/defaults.rb
+++ attributes/defaults.rb
@@ -9,7 +9,7 @@ default["couchpotato"]["group"] = 'couchpotato'
 default["couchpotato"]["directories"]["install"] = '/opt/couchpotato'
 default["couchpotato"]["directories"]["logs"] = '/var/log/couchpotato'
 default["couchpotato"]["directories"]["config"] = '/etc/couchpotato'
-default["couchpotato"]["directories"]["data"] = '/etc/couchpotato'
+default["couchpotato"]["directories"]["data"] = '/etc/couchpotato/data'
 
 default["couchpotato"]["git"]["url"] = "https://github.com/RuudBurger/CouchPotatoServer.git"
 default["couchpotato"]["git"]["tag"] = "master"
